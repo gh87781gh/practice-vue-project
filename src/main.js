e7d2262d19dcace6b3cda4220b16e1a9ffd62d0b
JennyShih <>
2019-08-27 15:35:31 +0800

    安裝 Vue-axios ，測試 api 傳送

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// 外部套件
 import Vue from 'vue'
-import App from './App'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+// 自定義
+import App from './App'
 
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
